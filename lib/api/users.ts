71de3c1c419892a5c153c307fc1e058e8513f2c9
Iryna Vysochan <ivysochan@gmail.com>
2026-03-29 16:40:19 +0300

    fix: extract user from api response

--- lib/api/users.ts
+++ lib/api/users.ts
@@ -2,7 +2,7 @@ import { nextServer } from './api';
 
 export const getUser = async (userId: string) => {
   const { data } = await nextServer.get(`/users/${userId}`);
-  return data;
+  return data.user;
 };
 
 export const getUserLocations = async (userId: string) => {
