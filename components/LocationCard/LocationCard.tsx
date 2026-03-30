9c8ae9fe18c72ca96331114462ac0950b44cfe78
Baramykova Jane <baramikovajane@gmail.com>
2026-03-30 11:03:55 +0300

    created component starrating through the library react-simple-star-rating

--- components/LocationCard/LocationCard.tsx
+++ components/LocationCard/LocationCard.tsx
@@ -2,12 +2,13 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import StarRating from "@/components/StarRating/StarRating";
 import css from "./LocationCard.module.css";
 import type { Location } from "@/types/location";
 
 interface Props {
   location: Location;
-  isOwner?: boolean; 
+  isOwner?: boolean;
 }
 
 export default function LocationCard({ location, isOwner }: Props) {
@@ -20,27 +21,18 @@ export default function LocationCard({ location, isOwner }: Props) {
           fill
           className={css.image}
         />
-
       </div>
 
       <div className={css.info}>
         <span className={css.tag}>
           {location.locationType}
         </span>
- <div className={css.rating}>
-    {Array.from({ length: 5 }, (_, i) => {
-      const full = i < Math.floor(location.rate);
-      const half = !full && i < location.rate;
-      return (
-        <span key={i}>{full ? "★" : half ? "⭐" : "☆"}</span>
-      );
-    })}
-  </div>
+
+        <StarRating value={location.rate} />
+
         <h3 className={css.title}>{location.name}</h3>
 
-        
         <div className={css.actions}>
-          
           <Link
             href={`/locations/${location._id}`}
             className={css.viewBtn}
@@ -50,14 +42,14 @@ export default function LocationCard({ location, isOwner }: Props) {
 
           {isOwner && (
             <Link
-  href={`/locations/${location._id}/edit`}
-  className={css.editBtn}
-  title="Редагувати"
->
-  <svg width="24" height="24">
-    <use href="/img/icons.svg#icon-edit" />
-  </svg>
-</Link>
+              href={`/locations/${location._id}/edit`}
+              className={css.editBtn}
+              title="Редагувати"
+            >
+              <svg width="24" height="24">
+                <use href="/img/icons.svg#icon-edit" />
+              </svg>
+            </Link>
           )}
         </div>
       </div>
