103333dffee6bb988822743b5e1f73d325598cdd
Dmytro Kitseliuk <dima_kitseliuk@ukr.net>
2026-04-01 22:57:27 +0300

    update api.ts

--- app/api/api.ts
+++ app/api/api.ts
@@ -2,8 +2,7 @@
 
 import axios from 'axios';
 
-const DEFAULT_API_URL = 'https://relax-map-api.onrender.com';
-const API_URL = (process.env.API_URL ?? DEFAULT_API_URL).replace(/\/$/, '');
+const API_URL = process.env.API_URL;
 
 export const api = axios.create({
   baseURL: `${API_URL}/api`,
