a6bf838d591c616eb2f98760f72c5e861f9f564c
Olena <marineelen91@gmail.com>
2026-04-05 11:26:01 +0300

    fixed url

--- app/locations/[locationId]/edit/page.tsx
+++ app/locations/[locationId]/edit/page.tsx
@@ -4,13 +4,13 @@ import type { Metadata } from 'next';
 import { fetchLocationById } from '@/lib/api/serverApi';
 import {notFound } from 'next/navigation';
 
-type EditLocationPageProps = {
+type EditLocationPageProps = {пше
   params: Promise<{
     locationId: string;
   }>;
 };
 
-const BASE_URL = 'https://project-group-01-fe.vercel.app';
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
 
 export async function generateMetadata(
   { params }: EditLocationPageProps
@@ -25,10 +25,10 @@ export async function generateMetadata(
     openGraph: {
       title: `Редагування: ${location.name}`,
       description: `Редагування інформації про локацію "${location.name}"`,
-      url: `${BASE_URL}/locations/action/edit/${locationId}`,
+      url: `${FRONTEND_URL}/locations/action/edit/${locationId}`,
       images: [
         {
-          url: location.image || `${BASE_URL}/og-image.jpg`,
+          url: location.image || `${FRONTEND_URL}/og-image.jpg`,
           width: 1200,
           height: 630,
           alt: location.name,
--- app/locations/action/add/page.tsx
+++ app/locations/action/add/page.tsx
@@ -4,7 +4,7 @@ import { Metadata } from 'next';
 
 
 
-const BASE_URL = 'https://project-group-01-fe.vercel.app';
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
 
 export const metadata: Metadata = {
   title: 'Створення локації',
@@ -13,10 +13,10 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'Створення локації',
     description: 'Сторінка для створення нової локації',
-    url: `${BASE_URL}/locations/action/add`,
+    url: `${FRONTEND_URL}/locations/action/add`,
     images: [
       {
-        url: `${BASE_URL}/og-image.jpg`,
+        url: `${FRONTEND_URL}/og-image.jpg`,
         width: 1200,
         height: 630,
         alt: 'Створення локації',
